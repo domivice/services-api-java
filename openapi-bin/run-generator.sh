861033d78a2cde160772b5436c9e32a53084a9c2
Nelson Kana <nelsonkana@gmail.com>
2023-11-30 20:06:15 -0500

    updates on bin files

--- openapi-bin/run-generator.sh
+++ openapi-bin/run-generator.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 @echo off
-java -jar generator-cli.jar generate -i specs.yaml -c configs.json -g spring -o ../
+java -jar openapi-generator-cli.jar generate -i specs.yaml -c configs.json -g spring -o ../
 echo Open api generator has been executed!
