7993de393e6099213aaaaf636730f5dc2a30edeb
Nelson Kana <nelsonkana@gmail.com>
2023-12-12 19:38:10 -0500

    open api integrated

--- services.api/services.web/src/main/java/org/domivice/services/web/Application.java
+++ services.api/services.web/src/main/java/org/domivice/services/web/Application.java
@@ -2,12 +2,13 @@ package org.domivice.services.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.domivice.services.openapi", "org.domivice.services.openapi.controllers" , "org.domivice.services.openapi.configurations"})
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
