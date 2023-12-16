382da32dfdbbf84f2d9fa6c612608fcaf47d9b97
Nelson Kana <nelsonkana@gmail.com>
2023-12-16 14:00:55 -0500

    first working version of add licence save working

--- services.api/services.application/src/main/java/org/domivice/services/application/common/commands/BaseCommand.java
+++ services.api/services.application/src/main/java/org/domivice/services/application/common/commands/BaseCommand.java
@@ -1,6 +1,8 @@
 package org.domivice.services.application.common.commands;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
@@ -8,6 +10,8 @@ import java.util.UUID;
 
 @Data
 @SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseCommand {
     @TargetAggregateIdentifier
     private UUID id;
