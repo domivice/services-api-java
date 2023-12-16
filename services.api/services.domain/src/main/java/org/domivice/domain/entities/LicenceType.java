d6dc79fb950290535b1558909e2c565a94a2ca45
Nelson Kana <nelsonkana@gmail.com>
2023-12-16 14:08:05 -0500

    minor change

--- services.api/services.domain/src/main/java/org/domivice/domain/entities/LicenceType.java
+++ services.api/services.domain/src/main/java/org/domivice/domain/entities/LicenceType.java
@@ -2,7 +2,6 @@ package org.domivice.domain.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
